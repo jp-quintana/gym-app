d638b34db50f7a72518104caf62b768726a31f63
jp-quintana <juanquintana1996@gmail.com>
2025-01-27 23:59:47 -0300

    add clear cookie in refresh guard

--- server/src/common/guards/refresh.guard.ts
+++ server/src/common/guards/refresh.guard.ts
@@ -16,11 +16,15 @@ export class RefreshGuard implements CanActivate {
   ) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
+    const response = context.switchToHttp().getResponse();
     const token = this.extractToken(request);
 
     if (!token) {
       throw new UnauthorizedException();
     }
+
+    response.clearCookie('refreshToken');
+
     try {
       const payload = await this.jwtService.verifyAsync(token, {
         secret: this.configService.get<string>('refreshTokenSecret'),
