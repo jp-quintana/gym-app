20178b9c1bd51f60860a4f4bad57b70ce3f368c7
jp-quintana <juanquintana1996@gmail.com>
2025-02-02 23:59:53 -0300

    add comment refresh service

--- server/src/modules/auth/auth.service.ts
+++ server/src/modules/auth/auth.service.ts
@@ -138,11 +138,11 @@ export class AuthService {
   }
 
   async refresh(req: UserRequest, res: Response) {
-    const user = await this.usersService.findOneByEmail(req.user.userId);
+    // const user = await this.usersService.findOneByEmail(req.user.userId);
 
-    if (user.deleted) {
-      throw new UnauthorizedException('User account deleted');
-    }
+    // if (user.deleted) {
+    //   throw new UnauthorizedException('User account deleted');
+    // }
 
     let authSession = await this.authSessionRepository.findOne({
       where: { refreshToken: req.user?.refreshToken, deleted: false },
