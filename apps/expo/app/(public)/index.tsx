dd5abef5d917f0ac63b8ecba927cdc65a7e62672
jp-quintana <juanquintana1996@gmail.com>
2025-02-25 23:59:33 -0300

    expo: wip index login screen

--- apps/expo/app/(public)/index.tsx
+++ apps/expo/app/(public)/index.tsx
@@ -1,5 +1,16 @@
+import { Button, Text } from '@/components/ui';
 import { View } from 'react-native';
+import { Image } from 'expo-image';
 
+// screen for google logo, banner & google log in
 export default function Index() {
-  return <View />;
+  return (
+    <View style={{ flex: 1, padding: 20 }}>
+      <Text>Replify</Text>
+      {/* <Image /> */}
+      <View>
+        <Button>Google login</Button>
+      </View>
+    </View>
+  );
 }
