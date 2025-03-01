86144908384cea1f69cd9db44d2d94868f2cda0c
jp-quintana <juanquintana1996@gmail.com>
2025-02-28 23:59:27 -0300

    expo: wip2 index login screen

--- apps/expo/app/(public)/index.tsx
+++ apps/expo/app/(public)/index.tsx
@@ -8,9 +8,12 @@ export default function Index() {
     <View style={{ flex: 1, padding: 20 }}>
       <Text>Replify</Text>
       {/* <Image /> */}
-      <View style={{ marginTop: 'auto' }}>
+      <View style={{ marginTop: 'auto', gap: 10 }}>
         <Button>
           <Text>Google login</Text>
+        </Button>
+        <Button>
+          <Text>Login</Text>
         </Button>
       </View>
     </View>
