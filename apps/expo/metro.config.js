f38af7fe8b239e657e11e4ef23c2523ceff554b4
jp-quintana <juanquintana1996@gmail.com>
2025-03-18 00:19:34 -0300

    expo: fix previous refactor errors

--- apps/expo/metro.config.js
+++ apps/expo/metro.config.js
@@ -1,9 +1,14 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 const exclusionList = require('metro-config/src/defaults/exclusionList');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
 config.resolver.blockList = exclusionList([/components\.json$/]);
 
-module.exports = withNativeWind(config, { input: './global.css' });
+config.resolver.extraNodeModules = {
+  '@': path.resolve(__dirname, 'src'),
+};
+
+module.exports = withNativeWind(config, { input: './src/global.css' });
