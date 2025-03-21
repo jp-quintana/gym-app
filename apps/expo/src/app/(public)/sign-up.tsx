8d1bcdc1710ec67d932240589ace5cb1dcf78425
jp-quintana <juanquintana1996@gmail.com>
2025-03-20 23:59:45 -0300

    expo: update public routes

---
+++ apps/expo/src/app/(public)/sign-up.tsx
@@ -0,0 +1,15 @@
+import { View } from 'react-native';
+
+export default function SignInScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      {/* <View style={{ flexDirection: 'row', gap: 10 }}></View> */}
+    </View>
+  );
+}
