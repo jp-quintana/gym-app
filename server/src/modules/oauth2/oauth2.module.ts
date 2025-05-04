4ad58a8b93fc40b8a156ec257f89ba8850e49324
jp-quintana <juanquintana1996@gmail.com>
2025-05-03 23:12:54 -0300

    server: wip oauth2 google

--- server/src/modules/oauth2/oauth2.module.ts
+++ server/src/modules/oauth2/oauth2.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { GoogleController } from './google/google.controller';
+import { GoogleService } from './google/google.service';
 
 @Module({
-  controllers: [GoogleController]
+  controllers: [GoogleController],
+  providers: [GoogleService]
 })
 export class Oauth2Module {}
